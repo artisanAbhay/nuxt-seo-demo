d150785c1e6dda47c2776665397f3ab31553836a
codextroop <codextroop@gmail.com>
2023-08-05 13:38:51 +0530

    next-seo-demo

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container-fluid">
+   <div class="row">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <NuxtLink to="/" :class="'navbar-brand'">
+                NuxtJS Gtag / meta
+            </NuxtLink>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <NuxtLink to="/" :class="'nav-link'" :active-class="'active'">
+                            Home
+                        </NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink :class="'nav-link'" to="/about" :active-class="'active'">About</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <slot />
+   </div>
+</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="col-lg-12 mt-2">
+    <div class="card">
+        <div class="card-header">
+            Home
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</div>
+</template>
+
+<script setup>
+const route = useRoute()
+const {
+    data,
+    pending,
+    error,
+    refresh
+} = await useAsyncData(
+    'meta',
+    () => $fetch('https://dev-xod6z6w15z02b67.api.raw-labs.com/mock/meta-tags',{
+    params: {
+      id: route.name
+    }
+  })
+)
+useSeoMeta({
+  title: () => data.value?.title,
+  description: () => data.value?.description,
+  keywords: () => data.value?.keywords,
+});
+</script>
